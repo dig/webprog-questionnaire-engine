65d145a67c5594f1a104fbd954805541b74b881c
Digital <josephpro42@live.co.uk>
2020-01-31 00:59:33 +0000

    Start logging

---
+++ src/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express'),
+      Log = require('./log');
+
+const port = process.env.PORT || 80;
+const isDev = process.env.DEV || false;
+
+const app = express();
+const logger = new Log(isDev);
+
+app.listen(port);
+logger.info(`Listening on port ${port}.`);
+
+module.exports = {
+  app,
+  logger
+};
