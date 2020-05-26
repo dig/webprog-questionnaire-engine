b1c3b6aec371cfc2c1c0c901fc44e964acf6f67c
Joseph <joseph42@live.co.uk>
2020-05-26 17:55:46 +0100

    Start create page

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,6 +4,7 @@ const router = new Router();
 // routes
 router.add('/', 'main/main.html', Main);
 router.add('/login', 'login/login.html', Login);
+router.add('/create', 'create/create.html', Create);
 router.add('*', 'notfound/notfound.html', NotFound);
 
 // main
