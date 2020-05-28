bca8074ba932111317a3915a7c0bf54866f2de2b
Joseph <joseph42@live.co.uk>
2020-05-28 12:10:11 +0100

    Add importing questionnaire

--- public/components/Page/Create.js
+++ public/components/Page/Create.js
@@ -20,17 +20,34 @@ class Create extends HTMLElement {
     fileInput.addEventListener('change', this.readFile, false);
   }
 
-  readFile(e) {
+  readFile = (e) => {
     let file = e.target.files[0];
     if (!file) return;
 
     let reader = new FileReader();
 
-    reader.onload = function(e) {
-      let contents = e.target.result;
-      console.log(contents);
-    };
+    reader.onload = (e) => this.createJSON(JSON.parse(e.target.result));
     reader.readAsText(file);
+  }
+
+  createJSON = async (object) => {
+    if (object.name && object.questions) {
+      try {
+        const response = await fetch('/api/questionnaire/json', {
+          method: 'PUT',
+          headers: {
+            'Authorization': `Bearer ${auth.getAccessToken()}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(object)
+        });
+
+        if (response.ok)
+          router.push('/questionnaires');
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 }
 
